d759ec11108ce279b2b18d1a245c2c20630f3932
Gavin Schilling <schillgc@hotmail.com>
2025-10-30 22:38:40 -0400

    Added API endpoints

--- renais_gin/bottles/urls.py
+++ renais_gin/bottles/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Web views
     path('my-bottles/', views.UserBottlesView.as_view(), name='user-bottles'),
     path('register/', views.BottleRegisterView.as_view(), name='register-bottle'),
     path('<str:bottle_id>/', views.BottleDetailView.as_view(), name='bottle-detail'),
+
+    # API endpoints for bottles.html frontend
+    path('api/register/', views.register_bottle_api, name='api-register-bottle'),
+    path('api/my-bottles/', views.my_bottles_api, name='api-my-bottles'),
 ]
