76cd1143336444f1669ef8222f4623f0ddef6fc6
Gavin Schilling <schillgc@hotmail.com>
2025-10-30 22:42:06 -0400

    Add Eligibility for Pledge Function

--- renais_gin/bottles/serializers.py
+++ renais_gin/bottles/serializers.py
@@ -10,7 +10,11 @@ class BottleBatchSerializer(serializers.ModelSerializer):
 
 class BottleSerializer(serializers.ModelSerializer):
     batch = BottleBatchSerializer(read_only=True)
+    is_eligible_for_pledge = serializers.SerializerMethodField()
 
     class Meta:
         model = Bottle
-        fields = ['bottle_id', 'batch', 'is_registered', 'registered_to']
+        fields = ['bottle_id', 'batch', 'is_registered', 'registered_to', 'is_eligible_for_pledge']
+
+    def get_is_eligible_for_pledge(self, obj):
+        return obj.is_registered and not obj.pledge_submitted
